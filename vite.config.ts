d7b11869417269829f064a5c336ce47f2fd5e66b
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2023-12-22 17:52:24 +0100

    feat: add support for inline styles with scss

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog({
+      vite: {
+        inlineStylesExtension: 'scss',
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
