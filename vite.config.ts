5e7952dde2f5319be90e8c69b294a399f2f31411
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2023-12-25 21:46:08 +0100

    feat: add path alias for folder that stores styles like mixins, variables, etc

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,9 @@ export default defineConfig(({ mode }) => ({
   },
   resolve: {
     mainFields: ['module'],
+    alias: {
+      '~': resolve(__dirname, 'src/app/styles'),
+    },
   },
   plugins: [
     analog({
