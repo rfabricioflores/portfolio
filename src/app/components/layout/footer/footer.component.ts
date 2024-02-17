8ea205e62f05ee3b3948638a46f82e22033d207d
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2024-02-17 12:26:41 +0100

    feat: footer boilerplate

---
+++ src/app/components/layout/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  template: ` <p>footer works!</p> `,
+})
+export class FooterComponent {}
