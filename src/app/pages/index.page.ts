5cf9350d2c48c7c4d6c10db631b97637298920bd
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-02-20 20:12:53 +0100

    feat: home page

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import HeaderComponent from '../components/common/header/header.component';
+import { AboutComponent } from '@components/common/about/about.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: ` <h1>Home</h1> `,
+  template: `
+    <app-header />
+    <app-about />
+  `,
+  imports: [HeaderComponent, AboutComponent],
+  styleUrl: './index.page.scss',
 })
 export default class HomeComponent {}
