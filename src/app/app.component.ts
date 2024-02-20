de61b011dd81c7a6c0cd6d6ee5669eab3815fc97
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-02-20 19:02:31 +0100

    refactor: use absolute paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/layout/navbar/navbar.component';
-import { FooterComponent } from './components/layout/footer/footer.component';
-
+import { FooterComponent } from '@components/layout/footer/footer.component';
+import { NavbarComponent } from '@components/layout/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
