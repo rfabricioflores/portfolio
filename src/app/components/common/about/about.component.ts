2bc64e32dc85be2024a35119b279b08909f3b03e
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-02-22 20:28:23 +0100

    feat: about section

---
+++ src/app/components/common/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+})
+export class AboutComponent {
+  @HostBinding('class')
+  readonly class = 'wrapper';
+}
