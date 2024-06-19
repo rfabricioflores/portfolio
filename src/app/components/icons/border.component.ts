75b26090e9c5712bc56c770aa225f3e0e4273007
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2024-06-19 13:12:39 +0200

    feat: border icon svg component

--- src/app/components/icons/border.component.ts
+++ src/app/components/icons/border.component.ts
@@ -1,42 +1,116 @@
-import { Component, input } from '@angular/core';
-
+import { Component, OnInit, input } from '@angular/core';
 @Component({
   selector: 'app-icon-border',
   standalone: true,
   template: `
     <svg
-      viewBox="0 0 462 465"
-      [attr.viewBox]="'0 0 ' + width() + ' ' + height()"
+      width="100%"
+      height="100%"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      [attr.width]="width()"
-      [attr.height]="height()"
     >
       <rect
-        x="0.5"
-        y="0.5"
-        [attr.width]="width() - 1"
-        [attr.height]="height() - 1"
-        rx="18.5"
-        stroke="url(#paint0_linear_6_249)"
+        [attr.x]="strokeWidth() / 2"
+        [attr.y]="strokeWidth() / 2"
+        [attr.rx]="borderRadius()"
+        [attr.stroke]="'url(#' + gradientId + ')'"
+        [attr.stroke-width]="strokeWidth()"
+        width="100%"
+        height="100%"
+        [style]="{
+          width: 'calc(100% - ' + strokeWidth() + 'px)',
+          height: 'calc(100% - ' + strokeWidth() + 'px)'
+        }"
       />
       <defs>
+        @switch(position()) { @case('up') {
         <linearGradient
-          id="paint0_linear_6_249"
-          x1="181"
-          y1="0"
-          x2="181"
-          [attr.y2]="width()"
+          [id]="gradientId"
+          x1="100%"
+          y1="0%"
+          x2="100%"
+          y2="100%"
           gradientUnits="userSpaceOnUse"
         >
           <stop stop-color="#B786F5" stop-opacity="0.77" />
           <stop offset="1" stop-color="#1196CF" />
         </linearGradient>
+        } @case('down'){
+        <linearGradient
+          [id]="gradientId"
+          x1="0%"
+          y1="100%"
+          x2="0%"
+          y2="0%"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#B786F5" stop-opacity="0.77" />
+          <stop offset="1" stop-color="#1196CF" />
+        </linearGradient>
+        } @case('diagonal'){
+        <linearGradient
+          [id]="gradientId"
+          x1="0%"
+          y1="0%"
+          x2="100%"
+          y2="100%"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#B786F5" stop-opacity="0.77" />
+          <stop offset="1" stop-color="#1196CF" />
+        </linearGradient>
+        } @case('right'){
+        <linearGradient
+          [id]="gradientId"
+          x1="0%"
+          y1="0%"
+          x2="100%"
+          y2="0%"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#B786F5" stop-opacity="0.77" />
+          <stop offset="1" stop-color="#1196CF" />
+        </linearGradient>
+        } @case('left') {
+        <linearGradient
+          [id]="gradientId"
+          x1="100%"
+          y1="0%"
+          x2="0%"
+          y2="0%"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#B786F5" stop-opacity="0.77" />
+          <stop offset="1" stop-color="#1196CF" />
+        </linearGradient>
+        } @case ('reverse-diagonal') {
+        <linearGradient
+          [id]="gradientId"
+          x1="0%"
+          y1="100%"
+          x2="100%"
+          y2="0%"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#B786F5" stop-opacity="0.77" />
+          <stop offset="1" stop-color="#1196CF" />
+        </linearGradient>
+        } }
       </defs>
     </svg>
   `,
+  styleUrl: './border.component.scss',
 })
-export default class BorderComponent {
-  width = input.required<number>();
-  height = input.required<number>();
+export default class BorderComponent implements OnInit {
+  public strokeWidth = input(2);
+  public borderRadius = input(12);
+  public position = input<
+    'up' | 'down' | 'diagonal' | 'reverse-diagonal' | 'right' | 'left'
+  >();
+  public gradientId: string = '';
+
+  ngOnInit() {
+    this.gradientId =
+      'linearGradient_' + Math.random().toString(36).substring(2, 9);
+  }
 }
