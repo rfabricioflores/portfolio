973ace590f1906edc113307a2646f9abac2abbe5
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2024-06-19 13:16:04 +0200

    feat: use home comps

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,7 +1,17 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component } from '@angular/core';
 import HeaderComponent from '../components/common/header/header.component';
 import { AboutComponent } from '@components/common/about/about.component';
 import ProjectsComponent from '@components/common/projects/projects.component';
+import { RouteMeta } from '@analogjs/router';
+import ExperienceComponent from '@components/common/experience/experience.component';
+import ContactComponent from '@components/common/contact/contact.component';
+
+export const routeMeta: RouteMeta = {
+  meta: [
+    { name: 'title', content: 'Fabricio Flores' },
+    { name: 'description', content: 'Fabricio Flores - Full Stack Developer' },
+  ],
+};
 
 @Component({
   selector: 'app-home',
@@ -10,8 +20,16 @@ import ProjectsComponent from '@components/common/projects/projects.component';
     <app-header />
     <app-about />
     <app-projects />
+    <app-experience />
+    <app-contact />
   `,
-  imports: [HeaderComponent, AboutComponent, ProjectsComponent],
+  imports: [
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    ContactComponent,
+  ],
   styleUrl: './index.page.scss',
 })
 export default class HomeComponent {}
