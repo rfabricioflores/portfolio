db69e01dfeb1f0616703f22c1e07d8cacfb24353
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2024-06-24 17:54:30 +0200

    feat: contact form with validation

--- src/app/components/common/contact/contact.component.ts
+++ src/app/components/common/contact/contact.component.ts
@@ -1,14 +1,61 @@
-import { Component, HostBinding } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, HostBinding, inject, signal } from '@angular/core';
 import BorderComponent from '@components/icons/border.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
-  imports: [BorderComponent],
+  imports: [BorderComponent, ReactiveFormsModule],
 })
 export default class ContactComponent {
   @HostBinding('class')
   readonly class = 'wrapper';
+
+  private http = inject(HttpClient);
+  private formBuilder = inject(FormBuilder);
+
+  public contactForm!: FormGroup;
+  public isLoading = signal(false);
+  public message = signal<string | null>(null);
+
+  constructor() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    if (this.contactForm.invalid) return;
+    this.message.set(null);
+    this.isLoading.set(true);
+
+    this.http
+      .post('https://api.fabricioflores.se/contact', this.contactForm.value)
+      .pipe(
+        finalize(() => {
+          this.isLoading.set(false);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.message.set("✅ Thank you! I'll be in touch with you.");
+          this.contactForm.reset();
+        },
+        error: (e) => {
+          console.log(e);
+          this.message.set('❌ Something went nuts, please try again.');
+        },
+      });
+  }
 }
