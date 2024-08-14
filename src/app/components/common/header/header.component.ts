ac493cf39f7456f6646088be04932991c175664b
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2024-08-14 12:38:45 +0200

    feat: custom animation to rotate tech icons

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, HostBinding } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  ElementRef,
+  HostBinding,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +15,53 @@ import { Component, HostBinding } from '@angular/core';
 export default class HeaderComponent {
   @HostBinding('class')
   readonly class = 'wrapper';
+
+  @ViewChild('techs', { static: false }) base!: ElementRef<HTMLElement>;
+
+  private radius: number = 0;
+  private techsWAngle: {
+    el: Element;
+    angle: number;
+  }[] = [];
+
+  constructor() {
+    afterNextRender(() => {
+      this.run();
+    });
+  }
+
+  private run() {
+    this.radius = this.base.nativeElement.offsetWidth / 2;
+    const techs = Array.from(this.base.nativeElement.children);
+
+    techs.forEach((item, index) => {
+      const angle = (index / techs.length) * 360;
+      this.techsWAngle.push({ el: item, angle });
+      this.calcPosition(item, angle);
+    });
+
+    this.base.nativeElement.style.opacity = '1';
+
+    this.techsWAngle.forEach((techWAngle) => {
+      setInterval(() => {
+        techWAngle.angle -= 5;
+        if (techWAngle.angle === 360) {
+          techWAngle.angle = 0;
+        }
+        this.calcPosition(techWAngle.el, techWAngle.angle);
+      }, 200);
+    });
+  }
+
+  private calcPosition(el: Element, angle: number) {
+    let xPos = Math.cos((angle * Math.PI) / 180) * this.radius;
+    let yPos = Math.sin((angle * Math.PI) / 180) * this.radius;
+    xPos += this.radius;
+    yPos += this.radius;
+
+    const style = `left: calc(${xPos}px - ${
+      el.clientWidth / 2
+    }px);top:calc(${yPos}px - ${el.clientWidth / 2}px);`;
+    el.setAttribute('style', style);
+  }
 }
