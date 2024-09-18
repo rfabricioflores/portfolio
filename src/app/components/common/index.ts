91696a6621ad3cf99db1f12e2a85f642a3ae916a
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-09-18 13:09:21 +0200

    feat: barrel for common components

---
+++ src/app/components/common/index.ts
@@ -0,0 +1,13 @@
+import AboutComponent from './about/about.component';
+import ContactComponent from './contact/contact.component';
+import ExperienceComponent from './experience/experience.component';
+import HeaderComponent from './header/header.component';
+import ProjectsComponent from './projects/projects.component';
+
+export {
+  AboutComponent,
+  ContactComponent,
+  ExperienceComponent,
+  HeaderComponent,
+  ProjectsComponent,
+};
