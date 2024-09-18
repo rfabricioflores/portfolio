a0a0e8d863a99bee2673017f6118cf73af33ed08
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-09-18 13:42:54 +0200

    feat: logo component

--- src/app/components/icons/index.ts
+++ src/app/components/icons/index.ts
@@ -1,5 +1,6 @@
 import BorderComponent from './border/border.component';
 import CloseComponent from './close/close.component';
+import LogoComponent from './logo/logo.component';
 import MenuComponent from './menu/menu.component';
 
-export { BorderComponent, CloseComponent, MenuComponent };
+export { BorderComponent, CloseComponent, MenuComponent, LogoComponent };
