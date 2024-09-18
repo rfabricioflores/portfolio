8c152bd85d9158127a93e79b2afd8c6b43c0d02f
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-09-18 13:43:31 +0200

    refactor: use logo comp, seo improvements.

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,18 +1,20 @@
-import { Component, HostBinding, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CloseComponent, MenuComponent } from '@components/icons';
+import {
+  CloseComponent,
+  LogoComponent,
+  MenuComponent,
+} from '@components/icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, MenuComponent, CloseComponent],
+  imports: [RouterLink, MenuComponent, CloseComponent, LogoComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
+  host: { class: 'wrapper' },
 })
 export class NavbarComponent {
-  @HostBinding('class')
-  readonly class = 'wrapper';
-
   public menuIsOpen = signal(false);
 
   public onLinkClick() {
