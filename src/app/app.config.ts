6ec279d84a7bb96e7e0f08ad2fb3ebea0035f18b
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-12-04 18:34:11 +0100

    feat: add latest configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideFileRouter } from '@analogjs/router';
+import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
 import { withComponentInputBinding } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideFileRouter(withComponentInputBinding()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([requestContextInterceptor])
+    ),
     provideClientHydration(),
   ],
 };
