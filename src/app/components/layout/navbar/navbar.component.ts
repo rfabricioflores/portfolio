248835b9108e19d4d826562bff64e59aba47319d
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-12-10 20:51:04 +0100

    refactor: optimize social media images

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {
@@ -9,7 +10,13 @@ import {
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, MenuComponent, CloseComponent, LogoComponent],
+  imports: [
+    RouterLink,
+    MenuComponent,
+    CloseComponent,
+    LogoComponent,
+    NgOptimizedImage,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   host: { class: 'wrapper' },
