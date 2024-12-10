d2020c9825cee379c0043b600bd2df443f2b0d7e
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-12-10 20:56:08 +0100

    refactor: optimize about image

--- src/app/components/common/about/about.component.ts
+++ src/app/components/common/about/about.component.ts
@@ -1,14 +1,13 @@
-import { Component, HostBinding } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
 import { BorderComponent } from '@components/icons';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [BorderComponent],
+  imports: [BorderComponent, NgOptimizedImage],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
+  host: { class: 'wrapper' },
 })
-export default class AboutComponent {
-  @HostBinding('class')
-  readonly class = 'wrapper';
-}
+export default class AboutComponent {}
